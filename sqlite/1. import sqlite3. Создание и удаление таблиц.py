77642f367b006d0b58fd8f017e569a34bd8708c6
ZeroCreator <59481361+ZeroCreator@users.noreply.github.com>
2022-04-12 18:17:48 +0300

    sql-1

--- sqlite/1. import sqlite3. Создание и удаление таблиц.py
+++ sqlite/1. import sqlite3. Создание и удаление таблиц.py
@@ -14,9 +14,10 @@ import sqlite3 as sq
 with sq.connect("saper.db") as con:
     cur = con.cursor()
 # Создание и удаление таблиц
-#    cur.execute("DROP TABLE users")
+    cur.execute("DROP TABLE IF EXISTS users")
 
     cur.execute("""CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
         sex INTEGER NOT NULL DEFAULT 1,
         old INTEGER,
