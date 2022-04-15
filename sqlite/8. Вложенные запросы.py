1178255a31a135a31af02178c00669fba214f34d
ZeroCreator <59481361+ZeroCreator@users.noreply.github.com>
2022-04-15 15:55:59 +0300

    вложенные SQL-запросы

--- sqlite/8. Вложенные запросы.py
+++ sqlite/8. Вложенные запросы.py
@@ -1,3 +1,4 @@
+# вложенные SQL-запросы
 import sqlite3 as sq
 
 with sq.connect("education.db") as con:
@@ -17,5 +18,37 @@ with sq.connect("education.db") as con:
         mark INTEGER 
     )""")
 
+    cur.execute("""CREATE TABLE IF NOT EXISTS female (
+        id INTEGER,
+        name TEXT,
+        sex INTEGER NOT NULL DEFAULT 2,
+        old INTEGER
+    )""")
+
 
 con.close()
+
+
+"""
+SELECT mark FROM marks
+WHERE id = 2 AND subject LIKE 'Си'
+
+SELECT name, subject, mark FROM marks
+JOIN students ON students.rowid = marks.id
+WHERE mark > 3 AND subject LIKE 'Си'
+
+==
+
+SELECT name, subject, mark FROM marks
+JOIN students ON students.rowid = marks.id
+WHERE mark > (SELECT mark FROM marks
+WHERE id = 2 AND subject LIKE 'Си')
+AND subject LIKE 'Си'
+
+# Вставить поля в другую таблицу
+INSERT INTO female
+SELECT * FROM students WHERE sex = 2
+
+INSERT INTO female
+SELECT NULL, name, sex, old FROM students WHERE sex = 2
+"""
